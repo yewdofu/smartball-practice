4bb13fe3bef9801fae15f91f5eec5dfb7252bcbe
Ricky <ricky@yewdofu.xyz>
2026-08-20 22:29:01 +0900

    add: full level unlock and death-time LR reload fix

--- src/code.asm
+++ src/code.asm
@@ -2,6 +2,19 @@ include
 
 !Empty = $00B330
 
+; enable controller inputs while 
+org $0092A4
+    db $80, $10
+
+org $008227
+    jsr unlock_every_levels
+
+org $00B350
+unlock_every_levels:
+    lda #$0F
+    sta $1E33
+    rts
+
 org !Empty
 every_frame_patch:
     ldx !frame_counter_t
